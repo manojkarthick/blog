ac6e771b89d187c1d68028e8e75730d64e650ac0
Manoj Karthick <manojkarthick@ymail.com>
2020-12-16 02:14:57 -0800

    Inline minified CSS and add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
@@ -34,6 +35,10 @@ module.exports = function (eleventyConfig) {
     const yyyy = page.date.getFullYear();
     const mm = String(page.date.getMonth() + 1).padStart(2, "0");
     return `${yyyy}/${mm}/${page.fileSlug}/`;
+  });
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 
   // Get the first `n` elements of a collection.
