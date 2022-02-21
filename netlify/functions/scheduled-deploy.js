7803b36929e7526f2da0af1b840258158d4e1796
Manoj Karthick <manojkarthick@users.noreply.github.com>
2022-02-21 11:49:29 -0800

    feat(deploy): add netlify scheduled function for deploy

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,5 +1,9 @@
+const fetch = require("node-fetch");
+const WEBHOOK_URL = process.env.SCHEDULED_DEPLOY_HOOK_URL;
+
 const handler = async function(event, context) {
-  console.log("Received event:", event)
+  console.log("Rebuilding site. Received event:", event)
+  await fetch(WEBHOOK_URL, {method: 'POST'});
 
   return {
     statusCode: 200,
